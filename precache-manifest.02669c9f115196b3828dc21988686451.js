719e045f1b00368fd0e418e47935c807a773c17f
Sebastian Laclé <info@slacle.com>
2019-10-18 23:52:12 -0500

    Updates

---
+++ precache-manifest.02669c9f115196b3828dc21988686451.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba971de0e0cc3dc792c4268a58d13a2d",
+    "url": "/country-capitals-game/index.html"
+  },
+  {
+    "revision": "9f2c35d0d45f8d1c29db",
+    "url": "/country-capitals-game/static/css/main.5214db9a.chunk.css"
+  },
+  {
+    "revision": "3562050f8e6dc0fc5a9b",
+    "url": "/country-capitals-game/static/js/2.0fc3f19d.chunk.js"
+  },
+  {
+    "revision": "9f2c35d0d45f8d1c29db",
+    "url": "/country-capitals-game/static/js/main.490d5940.chunk.js"
+  },
+  {
+    "revision": "e29b88a632346e30575e",
+    "url": "/country-capitals-game/static/js/runtime-main.7e5fa099.js"
+  }
+]);
